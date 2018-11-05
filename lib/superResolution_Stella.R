d287f757680528691a866604ff2ea77a017d3f93
Stella Chen <35880953+stella-chen28@users.noreply.github.com>
2018-11-04 19:16:56 -0500

    Update superResolution_Stella.R

--- lib/superResolution_Stella.R
+++ lib/superResolution_Stella.R
@@ -54,23 +54,3 @@ superResolution <- function(LR_dir, HR_dir, modelList){
 }
 
 
-for(i in 1:n_files){
-  imgLR <- readImage(paste0(LR_dir,  "img", "_", sprintf("%04d", i), ".jpg"))
-  pathHR <- paste0(HR_dir,  "img", "_", sprintf("%04d", i), ".jpg")
-  featMat <- array(NA, c(dim(imgLR)[1] * dim(imgLR)[2], 8, 3))
-  
-  ### step 1. for each pixel and each channel in imgLR:
-  ###           save (the neighbor 8 pixels - central pixel) in featMat
-  ###           tips: padding zeros for boundary points
-  
-  for(j in 1:3){
-    featMat[,,j]<-feat(imgLR[,,j])
-  }
-  ### step 2. apply the modelList over featMat
-  predMat <- test(modelList, featMat)
-  ### step 3. recover high-resolution from predMat and save in HR_dir
-  
-  out_img<-Image(predMat, colormode=Color) 
-  writeImage(out_img,file_name = pathHR)
-  #print(i)
-}
